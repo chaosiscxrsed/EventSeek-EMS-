eeba005008ebd0ac25a9dbb409b85fdc0bf8dee9
Samin Awale <awales@148@gmail.com>
2025-05-10 16:05:11 +0545

    minor issue

--- forgotps.php
+++ forgotps.php
@@ -5,7 +5,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Reset Password - EventSeek</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Gaegu&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="ems.css">
+    <script src="ems.js" defer></script>
 </head>
 <body>
     <div class="header-nav-container">
