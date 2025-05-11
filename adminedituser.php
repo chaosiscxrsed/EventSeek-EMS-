2106dc39d33b1d22998428ef03038a4ffa9328e1
Samin Awale <awales@148@gmail.com>
2025-05-11 12:14:44 +0545

    saved

--- adminedituser.php
+++ adminedituser.php
@@ -49,7 +49,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit User</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="admin.css">
